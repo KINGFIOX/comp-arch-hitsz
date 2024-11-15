703d0813fa6f232dbe1f1e41aa2ff72845869d2b
wangfiox <wangfiox@localhost.localdomain>
2024-11-15 20:06:12 +0800

    prefetch

--- src/lab2/gemm_kernel.h
+++ src/lab2/gemm_kernel.h
@@ -8,10 +8,10 @@
 #include <cstdint>
 
 extern "C" {
-    void gemm_kernel_baseline(float *C, float *A, float *B, uint64_t M, uint64_t N, uint64_t K);
+void gemm_kernel_baseline(float *C, float *A, float *B, uint64_t M, uint64_t N, uint64_t K);
 
-    void gemm_kernel_opt_loop(float *C, float *A, float *B, uint64_t M, uint64_t N, uint64_t K);
+void gemm_kernel_opt_loop(float *C, float *A, float *B, uint64_t M, uint64_t N, uint64_t K);
 
-    void gemm_kernel_opt_prefetch(float *C, float *A, float *B, uint64_t M, uint64_t N, uint64_t K);
+void gemm_kernel_opt_prefetch(float *C, float *A, float *B, uint64_t M, uint64_t N, uint64_t K);
 };
-#endif //COMPUTER_ARHITECTURE_LAB_GEMM_KERNEL_H
+#endif  // COMPUTER_ARHITECTURE_LAB_GEMM_KERNEL_H
